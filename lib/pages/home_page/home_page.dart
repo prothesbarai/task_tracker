e9e25f0f4d3d80393b4d5bddb6426257047d0a6e
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-11-26 11:22:56 +0600

    asp vb

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 import 'package:task_tracker/date_time_helper/date_time_helper.dart';
 import '../../firebase_auth/email_auth/provider/user_hive_provider.dart';
 import '../../utils/constant/app_colors.dart';
+import '../../widgets/list_items_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -150,9 +151,7 @@ class _HomePageState extends State<HomePage> {
   void backPress(){
     Navigator.pop(context);
   }
-  void sMessage(){
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Only one task can play at a time!")));
-  }
+
   @override
   void dispose() {
     taskNameController.dispose();
@@ -310,7 +309,7 @@ class _HomePageState extends State<HomePage> {
                 builder: (context, snapshot) {
                   if (!snapshot.hasData) {return const Center(child: CircularProgressIndicator());}
                   if (snapshot.data!.isEmpty) {return const Text("No Recent Activities Found");}
-                  return Column(children: snapshot.data!.map((tasks) {return _recentActivityCard(taskId: tasks["id"],projectName:tasks["projectName"],taskName: tasks["taskName"], createdAt: tasks["createdAt"], isPlaying: tasks["isPlaying"], status: tasks["status"],totalTime: tasks["singleTaskTotalPlayHour"] ?? "0",);}).toList(),);
+                  return Column(children: snapshot.data!.map((tasks) {return listItemsCard(context: context,taskId: tasks["id"],projectName:tasks["projectName"],taskName: tasks["taskName"], createdAt: tasks["createdAt"], isPlaying: tasks["isPlaying"], status: tasks["status"],totalTime: tasks["singleTaskTotalPlayHour"] ?? "0",);}).toList(),);
                 },
               ),
               /// <<< Recent Activity ==========================================
@@ -322,98 +321,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
-  /// >>> Recent Activity Component ============================================
-  Widget _recentActivityCard({required String projectName,required String taskId,required String taskName, required String createdAt, required bool isPlaying, required String status,required String totalTime}) {
-    int totalSeconds = int.tryParse(totalTime) ?? 0;
-    String durationText = DateTimeHelper.formatDuration(totalSeconds);
-    return StatefulBuilder(
-      builder: (context, setState) {
-        Color cardColor;
-        if (isPlaying) {cardColor = Colors.blue[100]!;} else if (status == "Paused") {cardColor = Colors.orange[100]!;} else if (status == "Completed") {cardColor = Colors.green[100]!;} else {cardColor = Colors.white;}
-        return Container(
-          margin: const EdgeInsets.only(bottom: 12),
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(color: cardColor, borderRadius: BorderRadius.circular(18),),
-          child: Row(
-            children: [
-              Icon(Icons.task, color: Colors.blue, size: 30),
-              const SizedBox(width: 12),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text.rich(
-                      TextSpan(
-                        children: [
-                          TextSpan(text: "$projectName : ", style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.black,),),
-                          TextSpan(text: taskName, style: const TextStyle(fontSize: 12, color: Colors.black38,),),
-                        ],
-                      ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    const SizedBox(height: 2),
-                    Text("$status | $createdAt",style: TextStyle(fontSize: 12, color: Colors.black38,),),
-                    const SizedBox(height: 2),
-                    Text("Time Played: $durationText", style: TextStyle(fontSize: 12, color: Colors.grey)),
-                  ],
-                ),
-              ),
-              if(status == "Playing" || status == "Paused" || status == "Assigned")...[
-                Row(
-                  children: [
-                    IconButton(
-                      icon: Icon(isPlaying ? Icons.pause_circle : Icons.play_circle, color: Colors.blue, size: 30,),
-                      onPressed: () async {
-                        final uid = FirebaseAuth.instance.currentUser!.uid;
-                        final taskRef = FirebaseDatabase.instance.ref("users/$uid/tasks");
-                        final snapshot = await taskRef.get();
-                        final tasksData = snapshot.value as Map?;
-                        if (tasksData == null) return;
-                        if (!isPlaying) {
-                          // Check if any other task is already playing
-                          bool anyOtherPlaying = tasksData.entries.any((entry) {final otherTask = Map<String, dynamic>.from(entry.value);return otherTask["isPlaying"] == true;});
-                          if (anyOtherPlaying) {
-                            if (!mounted) return;
-                            sMessage();
-                            return;
-                          }
-                          // Start the selected task
-                          final currentTimeStamp = DateTime.now().millisecondsSinceEpoch;
-                          await taskRef.child(taskId).update({"isPlaying": true, "lastPlayStartTime": currentTimeStamp,"status" : "Playing"});
-                          setState(() { isPlaying = true; });
-                        } else {
-                          // Pause the selected task
-                          final data = tasksData[taskId] as Map;
-                          int lastPlayStartTime = data["lastPlayStartTime"] ?? 0;
-                          int oldTotalSeconds = int.tryParse(data["singleTaskTotalPlayHour"] ?? "0") ?? 0;
-                          int addedSeconds = ((DateTime.now().millisecondsSinceEpoch - lastPlayStartTime) / 1000).round();
-                          int newTotalSeconds = oldTotalSeconds + addedSeconds;
-                          await taskRef.child(taskId).update({"isPlaying": false, "singleTaskTotalPlayHour": newTotalSeconds.toString(),"status" : "Paused"});
-                          setState(() { isPlaying = false; });
-                        }
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.check_circle, color: Colors.blue, size: 30,),
-                      onPressed: () async{
-                        final uid = FirebaseAuth.instance.currentUser!.uid;
-                        final taskRef = FirebaseDatabase.instance.ref("users/$uid/tasks");
-                        final snapshot = await taskRef.get();
-                        final tasksData = snapshot.value as Map?;
-                        if (tasksData == null) return;
-                        await taskRef.child(taskId).update({"isPlaying": false,"status" : "Completed"});
-                      },
-                    ),
-                  ],
-                )
-              ]
-            ],
-          ),
-        );
-      },
-    );
-  }
-/// <<< Recent Activity Component ============================================
 }
